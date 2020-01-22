933f13f9bdbe05a1f9ac290229f71703d7c34bcb
momotarou1090 <raurauji0123@gmail.com>
2020-01-22 18:03:26 +0900

    change

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+# config valid only for current version of Capistrano
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.11.2'
 
 # Capistranoのログの表示に利用する
@@ -14,7 +16,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['ChatSpace.pem'] 
+                  keys: ['~/.ssh/ChatSpace.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
